f6239f470473534a67c63d3d5e51f88051750c84
Rick Johnson <carl4917@umn.edu>
2021-11-21 17:22:17 -0600

    adding search and input functions

--- Assets/Style/script.js
+++ Assets/Style/script.js
@@ -17,3 +17,46 @@ let uviQuery = 'uvi?';
 let API_KEY = '2c5bfcf6504fdbc571e64d23b3bba150';
 const apiKey = '&appid=' + API_KEY;
 let searchHistory = {};
+
+$(document).ready(() => {
+	displaySearchHistory();
+});
+
+const displaySearchHistory = () => {
+	let searchHx = JSON.parse(localStorage.getItem('searchHistory'));
+	if (searchHx) {
+		arrayLength = searchHx.length;
+		for (let i = 0; i < arrayLength; ++i) {
+			$(`#row${i}`).html(
+				`<td><button class="recent btn btn-link p-0 text-muted">${searchHx[i].searchString}</button></td>`
+			);
+		}
+	}
+};
+
+$('table').on('click', 'button.recent', function () {
+	event.preventDefault();
+	getWeatherInfo($(this).text());
+});
+
+let initializeLocalStorage = () => {
+	localStorage.setItem('searchHistory', '[]');
+};
+
+$('#search-city').click(() => {
+	event.preventDefault();
+	let citySearchString = validatedSearchString(
+		$('input').attr('placeholder', 'city').val()
+	);
+	getWeatherInfo(citySearchString);
+});
+
+$('input').keypress((event) => {
+	if (event.which == 13) {
+		event.preventDefault();
+		let citySearchString = validatedSearchString(
+			$('input').attr('placeholder', 'city').val()
+		);
+		getWeatherInfo(citySearchString);
+	}
+});
